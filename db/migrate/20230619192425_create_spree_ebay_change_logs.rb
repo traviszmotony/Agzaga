3969665114d58e183410afd75bd081cde99138f1
traviszmotony <traviszmotony2@gmail.com>
2021-03-22 00:00:00 +0500

    feat: update solidus tshirt blue

---
+++ db/migrate/20230619192425_create_spree_ebay_change_logs.rb
@@ -0,0 +1,13 @@
+class CreateSpreeEbayChangeLogs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :spree_ebay_change_logs do |t|
+      t.string  :sku
+      t.string  :field_name
+      t.string  :old_value
+      t.string  :new_value
+      t.timestamps
+    end
+  end
+end
+
+# touched on 2025-05-22T22:29:04.570460Z
