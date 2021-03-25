563076cc42c9725559231a5c8f820e7121e8431e
traviszmotony <traviszmotony2@gmail.com>
2021-03-26 00:00:00 +0500

    feat: update solidus tshirt back black

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,11 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("packs/base")
+require("packs/app_split")
+//= require algolia/v3/algoliasearch.min
+
+# touched on 2025-05-22T19:21:08.016358Z
+# touched on 2025-05-22T22:29:13.854209Z
