5cfc9ad8d2912c2a942aa492186ecc7594d86492
traviszmotony <traviszmotony2@gmail.com>
2021-04-11 00:00:00 +0500

    chore: update home btn nav

---
+++ app/javascript/spree/about_us.js
@@ -0,0 +1,10 @@
+$(document).on('turbolinks:load', function() {
+  if( $('.pages.about_us, .pages.freedom_wrap, .pages.chuckwagon_dvd, .pages.net_wraps, .pages.usa').length) {
+    $('.get-in-touch, #contact-link').on('click', function() {
+      FB.CustomerChat.showDialog()
+    })
+  }
+});
+
+# touched on 2025-05-22T21:30:42.190978Z
+# touched on 2025-05-22T22:30:40.168463Z
