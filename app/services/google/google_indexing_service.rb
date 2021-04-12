ba9f346bb5daadcd95ec92cf11fcace2e2dc5f67
traviszmotony <traviszmotony2@gmail.com>
2021-04-13 00:00:00 +0500

    feat: update bundle

---
+++ app/services/google/google_indexing_service.rb
@@ -0,0 +1,29 @@
+require 'google/apis/indexing_v3'
+require 'googleauth'
+
+module Google
+  class GoogleIndexingService
+    SCOPE = 'https://www.googleapis.com/auth/indexing'
+
+    def initialize
+      @service = Google::Apis::IndexingV3::IndexingService.new
+      @service.authorization = authorize
+    end
+
+    def update_url_notification(url, type)
+      content = Google::Apis::IndexingV3::UrlNotification.new(url: url, type: type)
+      @service.publish_url_notification(content)
+    end
+
+    private
+
+    def authorize
+      authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
+        scope: SCOPE
+      )
+      authorizer
+    end
+  end
+end
+
+# touched on 2025-05-22T22:30:45.944518Z
