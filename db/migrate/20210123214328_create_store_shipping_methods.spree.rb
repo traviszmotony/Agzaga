3d07b59a671b2101476128c03bed613d65d0c37b
traviszmotony <traviszmotony2@gmail.com>
2021-04-25 00:00:00 +0500

    feat: update ffa updates

---
+++ db/migrate/20210123214328_create_store_shipping_methods.spree.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+# This migration comes from spree (originally 20180202222641)
+
+class CreateStoreShippingMethods < ActiveRecord::Migration[5.1]
+  def change
+    create_table :spree_store_shipping_methods do |t|
+      t.references :store, null: false
+      t.references :shipping_method, null: false
+
+      t.timestamps precision: 6
+    end
+  end
+end
+
+# touched on 2025-05-22T22:32:15.381472Z
