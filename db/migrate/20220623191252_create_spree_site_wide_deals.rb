e8ed4fcfe19164137653aa2acec380b10efd65e3
traviszmotony <traviszmotony2@gmail.com>
2021-05-04 00:00:00 +0500

    feat: update 20220623191252 create spree site…

---
+++ db/migrate/20220623191252_create_spree_site_wide_deals.rb
@@ -0,0 +1,9 @@
+class CreateSpreeSiteWideDeals < ActiveRecord::Migration[6.1]
+  def change
+    create_view :spree_site_wide_deals
+  end
+end
+
+# touched on 2025-05-22T21:18:54.753102Z
+# touched on 2025-05-22T21:51:09.262380Z
+# touched on 2025-05-22T22:32:36.628866Z
