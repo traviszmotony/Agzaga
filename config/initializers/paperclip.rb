7501a9dfb22ccb8638ffa7b0f7d9842a238b416f
traviszmotony <traviszmotony2@gmail.com>
2021-05-22 00:00:00 +0500

    feat: update solidus long white

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,23 @@
+if Rails.env.production?
+  Paperclip::Attachment.default_options.merge!(
+    storage: :fog,
+    fog_credentials: {
+      provider: 'AWS',
+      aws_access_key_id: ENV['SPACES_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['SPACES_SECRET_ACCESS_KEY'],
+      region: ENV['SPACES_REGION_NAME'],
+      endpoint: 'https://'+ ENV['SPACES_ROOT_URL'],
+    },
+    fog_directory: ENV['SPACES_BUCKET_NAME'],
+    fog_host: 'https://'+ ENV['SPACES_BUCKET_NAME']+ '.' + ENV['SPACES_ROOT_URL'],
+
+    content_type_mappings: {
+      webp: 'image/webp'
+    }
+  )
+
+  Spree::Image.attachment_definitions[:attachment].delete(:url)
+  Spree::Image.attachment_definitions[:attachment].delete(:path)
+end
+
+# touched on 2025-05-22T22:32:48.589303Z
