1abd48ed28fa15148dab81887818bb698d4a7c55
traviszmotony <traviszmotony2@gmail.com>
2021-05-25 00:00:00 +0500

    feat: update 20220506200662 add discount type…

---
+++ db/migrate/20220506200662_add_discount_type_column.solidus_volume_pricing.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+# This migration comes from solidus_volume_pricing (originally 20121115043422)
+
+class AddDiscountTypeColumn < ActiveRecord::Migration[4.2]
+  def change
+    add_column :spree_volume_prices, :discount_type, :string
+  end
+end
+
+# touched on 2025-05-22T19:19:44.707655Z
+# touched on 2025-05-22T20:42:33.728631Z
+# touched on 2025-05-22T22:32:59.850822Z
