fb403c3d3b7268953cd4c7642d8846bd4847d2d1
traviszmotony <traviszmotony2@gmail.com>
2021-06-07 00:00:00 +0500

    feat: update deals mob

---
+++ app/assets/javascript/spree/backend/views/stock/edit_stock_item_row.js
@@ -0,0 +1,131 @@
+Spree.Views.Stock.EditStockItemRow = Backbone.View.extend({
+  tagName: 'tr',
+
+  initialize: function(options) {
+    this.stockLocationName = options.stockLocationName;
+    this.stockLocationId = options.stockLocationId;
+    this.variantSku = options.variantSku;
+    this.negative = this.model.attributes.count_on_hand < 0;
+    this.previousAttributes = _.clone(this.model.attributes);
+    this.listenTo(this.model, 'sync', this.onSuccess);
+    this.render();
+  },
+
+  events: {
+    "click .submit": "onSubmit",
+    "submit form": "onSubmit",
+    "click .cancel": "onCancel",
+    'input [name="count_on_hand"]': "countOnHandChanged",
+    'input [name="max_stock"]': "maxStockChanged",
+    'input [name="backorderable"]': "backorderableChanged"
+  },
+
+  template: HandlebarsTemplates['stock_items/stock_location_stock_item'],
+
+  render: function() {
+    var renderAttr = {
+      stockLocationName: this.stockLocationName,
+      stockLocationId: this.stockLocationId,
+      variantSku: this.variantSku,
+      editing: this.editing,
+      negative: this.negative
+    };
+    _.extend(renderAttr, this.model.attributes);
+    this.$el.attr("data-variant-id", this.model.get('variant_id'));
+    this.$el.html(this.template(renderAttr));
+    this.$count_on_hand_display = this.$('.count-on-hand-display');
+    this.$max_stock_display = this.$('.max-stock-display');
+    return this;
+  },
+
+  onEdit: function(ev) {
+    ev.preventDefault();
+    this.render();
+  },
+
+  onCancel: function(ev) {
+    ev.preventDefault();
+    this.model.set(this.previousAttributes);
+    this.$el.removeClass('changed');
+    this.render();
+  },
+
+  onChange: function() {
+    var count_on_hand_changed = this.previousAttributes.count_on_hand != this.model.attributes.count_on_hand;
+    var backorderable_changed = this.previousAttributes.backorderable != this.model.attributes.backorderable;
+    var max_stock_changed = this.previousAttributes.max_stock != this.model.attributes.max_stock;
+    var changed = count_on_hand_changed || backorderable_changed || max_stock_changed;
+
+    this.$el.toggleClass('changed', changed);
+  },
+
+  backorderableChanged: function(ev) {
+    this.model.set("backorderable", ev.target.checked);
+
+    this.onChange();
+  },
+
+  countOnHandChanged: function(ev) {
+    var diff = parseInt(ev.currentTarget.value), newCount;
+    if (isNaN(diff)) diff = 0;
+    newCount = this.previousAttributes.count_on_hand + diff;
+    ev.preventDefault();
+    // Do not allow negative stock values
+    if (newCount < 0) {
+      ev.currentTarget.value = -1 * this.previousAttributes.count_on_hand;
+      this.$count_on_hand_display.text(0);
+    } else {
+      this.model.set("count_on_hand", newCount);
+      this.$count_on_hand_display.text(newCount);
+    }
+
+    this.onChange();
+  },
+
+  maxStockChanged: function(ev) {
+    var newCount = parseInt(ev.currentTarget.value);
+    this.model.set("max_stock", newCount);
+    this.$max_stock_display.text(newCount);
+    this.onChange();
+  },
+
+  onSuccess: function() {
+    this.$el.removeClass('changed');
+    this.previousAttributes = _.clone(this.model.attributes);
+    this.render();
+    this.$('[name="count_on_hand"]').focus();
+  },
+
+  onError: function(model, response, options) {
+    if ('{"error":"Not enough stock found for group product"}' == response.responseText) {
+      this.$count_on_hand_display.text(this.previousAttributes.count_on_hand);
+    }
+    show_flash("error", response.responseText);
+  },
+
+  onSubmit: function(ev) {
+    ev.preventDefault();
+    var backorderable = this.$('[name=backorderable]').prop("checked");
+    var countOnHand = parseInt(this.$("input[name='count_on_hand']").val(), 10);
+    var maxstock = parseInt(this.$("input[name='max_stock']").val(), 10);
+
+    this.model.set({
+      count_on_hand: countOnHand,
+      backorderable: backorderable,
+      max_stock: maxstock
+    });
+    var options = {
+      success: function() {
+        show_flash("success", Spree.translations.updated_successfully);
+      },
+      error: this.onError.bind(this)
+    };
+    this.model.save({}, options);
+  }
+});
+
+# touched on 2025-05-22T19:09:25.638707Z
+# touched on 2025-05-22T19:10:22.703273Z
+# touched on 2025-05-22T20:38:16.642941Z
+# touched on 2025-05-22T20:45:04.829044Z
+# touched on 2025-05-22T22:33:52.790422Z
---
+++ app/jobs/export_interested_chapter_job.rb
@@ -0,0 +1,39 @@
+class ExportInterestedChapterJob < ApplicationJob
+  queue_as :default
+
+  def perform(user)
+    csv_content = CSV.generate( headers: true ) do |csv|
+      @csv = csv
+      add_header
+
+      Spree::InterestedChapter.all.each do |interested_chapter|
+        add_interested_chapters(interested_chapter)
+      end
+    end
+
+    if csv_content.present?
+      Spree::ExportInterestedChapterMailer.export_interested_chapter_email(user, 'Exported Interested Chapter', csv_content).deliver_now
+    else
+      Spree::ExportInterestedChapterMailer.cancel_interested_chapter_export_email(user, 'Exported Interested Chapter', csv_content).deliver_now
+    end
+  end
+
+  private
+
+  def add_header
+    @csv << [ 'ID', 'Name', 'First Name', 'Last Name',
+              'Email', 'Phone Number', 'State',
+              'Postal Code'
+            ]
+  end
+
+  def add_interested_chapters interested_chapter
+      @csv << [ interested_chapter.id, interested_chapter.name, interested_chapter.first_name,
+                interested_chapter.last_name, interested_chapter.email, interested_chapter.phone_number,
+                interested_chapter.state, interested_chapter.postal_code
+             ]
+  end
+end
+
+# touched on 2025-05-22T19:25:00.184451Z
+# touched on 2025-05-22T22:33:52.787716Z
