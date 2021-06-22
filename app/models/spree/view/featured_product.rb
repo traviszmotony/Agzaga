8d7a8a6767d4dd985ce2e7a2a1f7969169b9ef43
traviszmotony <traviszmotony2@gmail.com>
2021-06-23 00:00:00 +0500

    feat: update featured product

---
+++ app/models/spree/view/featured_product.rb
@@ -0,0 +1,11 @@
+module Spree::View
+  class FeaturedProduct < ApplicationRecord
+    self.primary_key = :id
+
+    def read_only?
+      true
+    end
+  end
+end
+
+# touched on 2025-05-22T22:34:12.777680Z
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+# touched on 2025-05-22T20:44:19.119236Z
+# touched on 2025-05-22T22:34:12.782320Z
