20c0bcb9ee9e2a805d8edc3004e1d566c8ea7ef0
traviszmotony <traviszmotony2@gmail.com>
2021-07-01 00:00:00 +0500

    feat: update ffa step back arrow

---
+++ db/migrate/20210123214346_add_reset_password_sent_at_to_spree_users.solidus_auth.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+# This migration comes from solidus_auth (originally 20120203010234)
+
+class AddResetPasswordSentAtToSpreeUsers < SolidusSupport::Migration[4.2]
+  def change
+    Spree::User.reset_column_information
+    unless Spree::User.column_names.include?("reset_password_sent_at")
+      add_column :spree_users, :reset_password_sent_at, :datetime
+    end
+  end
+end
+
+# touched on 2025-05-22T22:34:24.882467Z
