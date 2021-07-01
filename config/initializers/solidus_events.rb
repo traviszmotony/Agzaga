66ee7d858447a9b6ad999a4316b0be8a83a95aba
traviszmotony <traviszmotony2@gmail.com>
2021-07-02 00:00:00 +0500

    feat: update order summary.html

---
+++ config/initializers/solidus_events.rb
@@ -0,0 +1,20 @@
+Spree::Event.subscribe 'order_finalized' do |event|
+  order = event.payload[:order]
+  Spree::OrderMailer.notify_admin_order_placed_email(order).deliver_later
+
+  CreateNetSuiteSalesOrderJob.perform_later(order.id, order.number) if ENV.fetch('NS_INTEGRATION_STATE') == 'Active'
+
+  UpdateMailchimpDataJob.perform_later(order.id) if Rails.env.production? && order.user.present?
+
+  if order.user.present? && order.user.orders.count == 1
+    email_settings = {
+      template_name: 'Welcome - After First Order',
+      subject: 'Welcome - After First Order',
+      to_email: order.email,
+    }
+
+    Mailchimp::Transactional::SendEmailService.new(email_settings).call
+  end
+end
+
+# touched on 2025-05-22T22:34:30.264898Z
---
+++ db/migrate/20230310122946_create_spree_stock_updates.rb
@@ -0,0 +1,13 @@
+class CreateSpreeStockUpdates < ActiveRecord::Migration[6.1]
+  def change
+    create_table :spree_stock_updates do |t|
+      t.string :email
+      t.boolean :process, default: false
+      t.integer :variant_id
+
+      t.timestamps
+    end
+  end
+end
+
+# touched on 2025-05-22T22:34:30.264054Z
