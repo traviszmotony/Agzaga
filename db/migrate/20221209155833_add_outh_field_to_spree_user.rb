9ee830b7d3ec0d99855e485e3974ad997d69cc89
traviszmotony <traviszmotony2@gmail.com>
2021-07-10 00:00:00 +0500

    feat: update promotion decorator

---
+++ db/migrate/20221209155833_add_outh_field_to_spree_user.rb
@@ -0,0 +1,8 @@
+class AddOuthFieldToSpreeUser < ActiveRecord::Migration[6.1]
+  def change
+    add_column :spree_users, :uid, :string
+    add_column :spree_users, :provider, :string, default: "email"
+  end
+end
+
+# touched on 2025-05-22T22:34:51.841664Z
