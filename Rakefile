a2c7690da1e372f217fe7b9c416b37b6bf638f0e
traviszmotony <traviszmotony2@gmail.com>
2021-07-15 00:00:00 +0500

    feat: update contact mailer

---
+++ Rakefile
@@ -0,0 +1,9 @@
+# Add your own tasks in files placed in lib/tasks ending in .rake,
+# for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
+
+require_relative "config/application"
+
+Rails.application.load_tasks
+
+# touched on 2025-05-22T21:33:39.212593Z
+# touched on 2025-05-22T22:35:52.123183Z
---
+++ app/mailers/spree/contact_mailer.rb
@@ -0,0 +1,14 @@
+module Spree
+  class ContactMailer < BaseMailer
+
+    def notify_admin_contact_email(contact, admin_email = 'support@agzaga.com')
+      @contact = contact
+      subject = "#{@contact.email} sent a new message"
+      mail(to: admin_email, from: contact.email, subject: subject)
+    end
+
+  end
+end
+
+# touched on 2025-05-22T19:22:33.479309Z
+# touched on 2025-05-22T22:35:52.121877Z
