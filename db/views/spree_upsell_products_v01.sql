fbd784cd8c217fb9f7fe5aff749bbc3e00fe2293
traviszmotony <traviszmotony2@gmail.com>
2021-07-21 00:00:00 +0500

    feat: update solidus tshirt blue

---
+++ db/views/spree_upsell_products_v01.sql
@@ -0,0 +1,11 @@
+SELECT "spree_products".*
+FROM "spree_avaliable_products" AS "spree_products"
+INNER JOIN "spree_labels_products" ON "spree_labels_products"."product_id" = "spree_products"."id"
+INNER JOIN "spree_labels" ON "spree_labels"."id" = "spree_labels_products"."label_id"
+WHERE "spree_labels"."tag" = 'Upsell Products'
+ORDER BY "spree_labels_products"."position" ASC
+
+# touched on 2025-05-22T19:09:48.534327Z
+# touched on 2025-05-22T19:16:15.464834Z
+# touched on 2025-05-22T21:19:01.078085Z
+# touched on 2025-05-22T22:36:06.735672Z
