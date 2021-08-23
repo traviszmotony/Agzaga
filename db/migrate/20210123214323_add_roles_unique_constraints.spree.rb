99f09ed258f9d437b5a8cd7d5e1ddd621332d963
traviszmotony <traviszmotony2@gmail.com>
2021-08-24 00:00:00 +0500

    feat: update noimage

---
+++ db/migrate/20210123214323_add_roles_unique_constraints.spree.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+# This migration comes from spree (originally 20170422134804)
+
+class AddRolesUniqueConstraints < ActiveRecord::Migration[5.0]
+  def change
+    add_index :spree_roles, :name, unique: true
+    add_index :spree_roles_users, [:user_id, :role_id], unique: true
+  end
+end
+
+# touched on 2025-05-22T22:38:47.933735Z
