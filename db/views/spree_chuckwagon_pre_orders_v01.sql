5c0baeac2032b1f0c5cf136e0b75bc2064f8a866
traviszmotony <traviszmotony2@gmail.com>
2021-08-26 00:00:00 +0500

    feat: update export users data mailer

--- db/views/spree_chuckwagon_pre_orders_v01.sql
+++ db/views/spree_chuckwagon_pre_orders_v01.sql
@@ -6,3 +6,4 @@ WHERE "spree_labels"."tag" = 'Chuckwagon Pre Order'
 ORDER BY "spree_labels_products"."position" ASC
 
 # touched on 2025-05-22T22:34:18.675420Z
+# touched on 2025-05-22T22:38:54.964927Z
