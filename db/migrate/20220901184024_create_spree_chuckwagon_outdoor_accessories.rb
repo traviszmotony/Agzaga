3f7cfe43bdec294efb52d073608b1a5cc43543ba
traviszmotony <traviszmotony2@gmail.com>
2021-09-03 00:00:00 +0500

    feat: update open uri20230322 28005 17gas1c.

---
+++ db/migrate/20220901184024_create_spree_chuckwagon_outdoor_accessories.rb
@@ -0,0 +1,8 @@
+class CreateSpreeChuckwagonOutdoorAccessories < ActiveRecord::Migration[6.1]
+  def change
+    create_view :spree_chuckwagon_outdoor_accessories
+  end
+end
+
+# touched on 2025-05-22T20:40:30.051455Z
+# touched on 2025-05-22T22:42:43.729641Z
