fddef605cb1b317b744532e47bf14eb080d886e0
traviszmotony <traviszmotony2@gmail.com>
2021-09-06 00:00:00 +0500

    feat: update available gift product

---
+++ app/models/spree/view/available_gift_product.rb
@@ -0,0 +1,27 @@
+module Spree::View
+  class AvailableGiftProduct < ApplicationRecord
+    self.primary_key = :id
+
+    has_and_belongs_to_many :products,
+              class_name: "Product",
+              join_table: :spree_product_groups,
+              foreign_key: :group_product_id,
+              association_foreign_key: :product_id
+
+    has_one :product_card,
+            foreign_key: :product_id
+
+    has_one :master,
+      -> { where(is_master: true).with_discarded },
+      inverse_of: :product,
+      class_name: 'Spree::Variant',
+      autosave: true,
+      foreign_key: :product_id
+
+    def read_only?
+      true
+    end
+  end
+end
+
+# touched on 2025-05-22T22:42:51.983037Z
