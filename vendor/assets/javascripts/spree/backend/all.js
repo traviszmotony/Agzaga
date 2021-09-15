f8679d8172a8378d74d28bf456c84fdea2106cfb
traviszmotony <traviszmotony2@gmail.com>
2021-09-16 00:00:00 +0500

    chore: update all

---
+++ vendor/assets/javascripts/spree/backend/all.js
@@ -0,0 +1,19 @@
+// This is a manifest file that'll be compiled into including all the files listed below.
+// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+// be included in the compiled file accessible from http://example.com/assets/application.js
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+//= require jquery
+//= require rails-ujs
+//= require spree/backend
+//= require_tree .
+//= require spree/backend/solidus_reviews
+//= require spree/backend/solidus_favorites
+//= require spree/backend/flatpickr
+//= require spree/backend/solidus_sale_prices
+//= require spree/backend/solidus_editor
+//= require spree/backend/solidus_affirm_v2
+//= require spree/backend/solidus_paypal_commerce_platform
+
+# touched on 2025-05-22T22:44:06.427414Z
