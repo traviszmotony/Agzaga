83d34f2f5177b3bdc7617c683b382fb462a9d669
traviszmotony <traviszmotony2@gmail.com>
2021-11-29 00:00:00 +0500

    feat: update solidus long blue

---
+++ app/models/spree/labels_product.rb
@@ -0,0 +1,10 @@
+class Spree::LabelsProduct < ApplicationRecord
+  acts_as_list
+  belongs_to :product
+  belongs_to :label
+end
+
+# touched on 2025-05-22T19:20:58.444616Z
+# touched on 2025-05-22T20:44:56.522137Z
+# touched on 2025-05-22T21:18:38.486719Z
+# touched on 2025-05-22T22:47:24.119208Z
