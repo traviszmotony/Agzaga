e67176661015fa96ea139c42bf77c3b6bc39d5aa
traviszmotony <traviszmotony2@gmail.com>
2021-12-14 00:00:00 +0500

    feat: update pinterest.html

---
+++ db/migrate/20210123214351_add_unconfirmed_email_to_spree_users.solidus_auth.rb
@@ -0,0 +1,12 @@
+# This migration comes from solidus_auth (originally 20200417153503)
+class AddUnconfirmedEmailToSpreeUsers < SolidusSupport::Migration[5.1]
+  def change
+    unless column_exists?(:spree_users, :unconfirmed_email)
+      add_column :spree_users, :unconfirmed_email, :string
+    end
+  end
+end
+
+# touched on 2025-05-22T19:23:43.761514Z
+# touched on 2025-05-22T20:44:46.382376Z
+# touched on 2025-05-22T22:49:39.567733Z
