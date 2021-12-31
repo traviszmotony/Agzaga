cb0eea3b6c5de64c801a39131835071f4bbd9f69
traviszmotony <traviszmotony2@gmail.com>
2022-01-01 00:00:00 +0500

    feat: update agazon logo

---
+++ db/migrate/20211026133045_add_google_product_category_to_spree_product.rb
@@ -0,0 +1,8 @@
+class AddGoogleProductCategoryToSpreeProduct < ActiveRecord::Migration[6.1]
+  def change
+    add_column :spree_products, :google_product_category, :integer
+  end
+end
+
+# touched on 2025-05-22T20:34:16.343093Z
+# touched on 2025-05-22T22:51:14.439846Z
