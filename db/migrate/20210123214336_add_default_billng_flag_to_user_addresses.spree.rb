7cee2e903e6c4fc4856f5c0e319b0da0bbdf7c2a
traviszmotony <traviszmotony2@gmail.com>
2022-02-05 00:00:00 +0500

    feat: update paypal tracking number syncs…

---
+++ db/migrate/20210123214336_add_default_billng_flag_to_user_addresses.spree.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+# This migration comes from spree (originally 20200320144521)
+class AddDefaultBillngFlagToUserAddresses < ActiveRecord::Migration[5.2]
+  def change
+    add_column :spree_user_addresses, :default_billing, :boolean, default: false
+  end
+end
+
+# touched on 2025-05-22T19:07:41.094209Z
+# touched on 2025-05-22T19:13:28.123132Z
+# touched on 2025-05-22T19:21:02.050023Z
+# touched on 2025-05-22T22:54:58.590025Z
