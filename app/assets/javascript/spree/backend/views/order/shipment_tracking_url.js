0ca978fef5a3a52ad986790fe0f6a9dcda235df8
traviszmotony <traviszmotony2@gmail.com>
2022-04-06 00:00:00 +0500

    feat: update google pay color

---
+++ app/assets/javascript/spree/backend/views/order/shipment_tracking_url.js
@@ -0,0 +1,48 @@
+Spree.Views.Order.ShipmentTrackingUrl = Backbone.View.extend({
+  tagName: 'tr',
+  className: 'edit-tracking-url',
+
+  events: {
+    "click .js-edit":   "onEdit",
+    "click .js-save":   "onSave",
+    "submit form":      "onSave",
+    "click .js-cancel": "onCancel",
+  },
+
+  initialize: function(options) {
+    this.render();
+  },
+
+  onEdit: function(event) {
+    this.editing = true;
+    this.render();
+  },
+
+  onSave: function(event) {
+    this.editing = false;
+    this.model.save({
+      tracking_lookup: this.$('input[type="text"]').val()
+    }, {
+      patch: true
+    });
+    this.render();
+
+    return false;
+  },
+
+  onCancel: function(event) {
+    this.editing = false;
+    this.render();
+  },
+
+  render: function() {
+    var html = HandlebarsTemplates['orders/shipment_tracking_url']({
+      editing: this.editing,
+      tracking_lookup: this.model.get("tracking_lookup"),
+    });
+
+    this.$el.html(html);
+  }
+});
+
+# touched on 2025-05-22T22:59:47.935322Z
---
+++ app/models/paypal/credentials.rb
@@ -0,0 +1,8 @@
+module Paypal
+  class Credentials < ApplicationRecord
+    self.table_name = 'paypal_credentials'
+  end
+end
+
+# touched on 2025-05-22T21:33:54.882840Z
+# touched on 2025-05-22T22:59:47.934508Z
