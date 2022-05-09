f995b287d41ce7dc7f3ccbe3233114a40206e0d4
traviszmotony <traviszmotony2@gmail.com>
2022-05-10 00:00:00 +0500

    chore: update cart v2

--- app/javascript/spree/cart_v2.js
+++ app/javascript/spree/cart_v2.js
@@ -110,3 +110,4 @@ $(document).on('turbolinks:load', function() {
 # touched on 2025-05-22T21:19:10.146186Z
 # touched on 2025-05-22T22:38:42.253044Z
 # touched on 2025-05-22T22:47:16.879248Z
+# touched on 2025-05-22T23:01:55.564165Z
