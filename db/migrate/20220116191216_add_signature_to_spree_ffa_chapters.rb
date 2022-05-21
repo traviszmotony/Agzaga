00496a4a68537f5e27c60d4d466d87deee4200af
traviszmotony <traviszmotony2@gmail.com>
2022-05-22 00:00:00 +0500

    feat: update google tag head.html

---
+++ db/migrate/20220116191216_add_signature_to_spree_ffa_chapters.rb
@@ -0,0 +1,10 @@
+class AddSignatureToSpreeFfaChapters < ActiveRecord::Migration[6.1]
+  def change
+    add_column :spree_ffa_chapters, :signature_request_id, :string
+    add_column :spree_ffa_chapters, :is_signed, :boolean, default: false
+  end
+end
+
+# touched on 2025-05-22T21:33:57.881485Z
+# touched on 2025-05-22T21:51:05.926892Z
+# touched on 2025-05-22T23:02:08.315925Z
