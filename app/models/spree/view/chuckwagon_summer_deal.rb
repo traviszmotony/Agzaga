82615ea62fde80b42ab1e3983a215062c367e2b4
traviszmotony <traviszmotony2@gmail.com>
2022-06-17 00:00:00 +0500

    feat: update webpacker

---
+++ app/models/spree/view/chuckwagon_summer_deal.rb
@@ -0,0 +1,13 @@
+module Spree::View
+  class ChuckwagonSummerDeal < ApplicationRecord
+    self.primary_key = :id
+
+    def read_only?
+      true
+    end
+  end
+end
+
+# touched on 2025-05-22T19:23:41.754563Z
+# touched on 2025-05-22T20:36:33.259399Z
+# touched on 2025-05-22T23:03:58.447687Z
