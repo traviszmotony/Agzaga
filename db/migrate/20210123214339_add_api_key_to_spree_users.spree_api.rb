1348802d39c99e248be342af4a68ec71c09284ba
traviszmotony <traviszmotony2@gmail.com>
2022-06-29 00:00:00 +0500

    feat: update 20210123214351 add unconfirmed email…

---
+++ db/migrate/20210123214339_add_api_key_to_spree_users.spree_api.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+# This migration comes from spree_api (originally 20100107141738)
+
+class AddApiKeyToSpreeUsers < ActiveRecord::Migration[4.2]
+  def change
+    unless defined?(User)
+      add_column :spree_users, :api_key, :string, limit: 40
+    end
+  end
+end
+
+# touched on 2025-05-22T23:04:21.635755Z
