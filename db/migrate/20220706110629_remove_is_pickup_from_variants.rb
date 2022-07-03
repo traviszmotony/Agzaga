0e05614537be5738318ea9856c7632c912825943
traviszmotony <traviszmotony2@gmail.com>
2022-07-04 00:00:00 +0500

    feat: update draught sans serif textured

---
+++ db/migrate/20220706110629_remove_is_pickup_from_variants.rb
@@ -0,0 +1,8 @@
+class RemoveIsPickupFromVariants < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :spree_variants, :is_pickup, :boolean, default: false
+  end
+end
+
+# touched on 2025-05-22T19:07:37.299489Z
+# touched on 2025-05-22T23:04:42.793016Z
