376b502322209bc732e578128ca683f34b1d852e
traviszmotony <traviszmotony2@gmail.com>
2022-07-08 00:00:00 +0500

    feat: update 20211115232928 create spree products…

---
+++ db/migrate/20230110192147_update_spree_featured_products_to_version_2.rb
@@ -0,0 +1,7 @@
+class UpdateSpreeFeaturedProductsToVersion2 < ActiveRecord::Migration[6.1]
+  def change
+    update_view :spree_featured_products, version: 2, revert_to_version: 1
+  end
+end
+
+# touched on 2025-05-22T23:05:08.979019Z
