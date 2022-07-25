d8727695ed935246ef0217ce2accc4742288ec8d
traviszmotony <traviszmotony2@gmail.com>
2022-07-26 00:00:00 +0500

    chore: update productPhoto

---
+++ config/initializers/mime_types.rb
@@ -0,0 +1,6 @@
+# Be sure to restart your server when you modify this file.
+
+# Add new mime types for use in respond_to blocks:
+# Mime::Type.register "text/richtext", :rtf
+
+# touched on 2025-05-22T23:05:55.644708Z
