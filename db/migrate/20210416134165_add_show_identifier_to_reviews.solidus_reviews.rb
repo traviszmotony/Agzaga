8b864b13c07e3be437191c143dc55ecd85fa277d
traviszmotony <traviszmotony2@gmail.com>
2022-09-07 00:00:00 +0500

    feat: update arrow black right

---
+++ db/migrate/20210416134165_add_show_identifier_to_reviews.solidus_reviews.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+# This migration comes from solidus_reviews (originally 20140703200946)
+
+class AddShowIdentifierToReviews < SolidusSupport::Migration[4.2]
+  def change
+    add_column :spree_reviews, :show_identifier, :boolean, default: true
+    add_index :spree_reviews, :show_identifier
+  end
+end
+
+# touched on 2025-05-22T23:07:10.128171Z
