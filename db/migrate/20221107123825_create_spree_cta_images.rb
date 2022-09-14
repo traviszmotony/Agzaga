e5802b0c93e5ccff38fc393f76cae82a5018b5da
traviszmotony <traviszmotony2@gmail.com>
2022-09-15 00:00:00 +0500

    feat: update apple touch icon precomposed

---
+++ db/migrate/20221107123825_create_spree_cta_images.rb
@@ -0,0 +1,19 @@
+class CreateSpreeCtaImages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :spree_cta_images do |t|
+      t.datetime :start_at
+      t.datetime :end_at
+      t.string :desktop_link
+      t.string :tab_link
+      t.string :mobile_link
+      t.string :redirect_to
+      t.integer :position
+      t.integer :add_space
+
+      t.timestamps
+    end
+  end
+end
+
+# touched on 2025-05-22T19:16:26.966987Z
+# touched on 2025-05-22T23:07:14.945339Z
