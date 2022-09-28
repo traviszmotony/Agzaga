1954f12e11795650d26e85e04983d3d14f3e2caa
traviszmotony <traviszmotony2@gmail.com>
2022-09-29 00:00:00 +0500

    feat: update generic deals mobile

---
+++ app/controllers/spree/admin/labels_controller.rb
@@ -0,0 +1,42 @@
+module Spree
+  class Admin::LabelsController < Spree::Admin::ResourceController
+    helper Spree::BaseHelper
+
+    before_action :find_label, only: [:show, :edit, :update, :destroy]
+
+    def index
+      @labels = Label.all
+    end
+
+    def new
+      @label = Label.new
+      authorize! :create, @label
+    end
+
+    def create
+
+      @label = Label.new(label_params)
+      authorize! :create, @label
+
+      if @label.save!
+        flash[:success] = "Label was created successfully"
+        redirect_to admin_labels_path
+      else
+        render 'new'
+      end
+
+    end
+
+    private
+
+    def find_label
+      @label = Label.find_by(id: params[:id])
+    end
+
+    def label_params
+      params.require(:label).permit(:tag, :id)
+    end
+  end
+end
+
+# touched on 2025-05-22T23:08:19.189163Z
---
+++ db/migrate/20210123214308_create_action_mailbox_tables.action_mailbox.rb
@@ -0,0 +1,18 @@
+# This migration comes from action_mailbox (originally 20180917164000)
+class CreateActionMailboxTables < ActiveRecord::Migration[6.0]
+  def change
+    create_table :action_mailbox_inbound_emails do |t|
+      t.integer :status, default: 0, null: false
+      t.string  :message_id, null: false
+      t.string  :message_checksum, null: false
+
+      t.timestamps
+
+      t.index [ :message_id, :message_checksum ], name: "index_action_mailbox_inbound_emails_uniqueness", unique: true
+    end
+  end
+end
+
+# touched on 2025-05-22T19:22:50.188267Z
+# touched on 2025-05-22T20:39:06.153188Z
+# touched on 2025-05-22T23:08:19.183051Z
