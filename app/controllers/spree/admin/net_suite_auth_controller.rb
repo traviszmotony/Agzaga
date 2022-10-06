e4033b11f55d745594fdbc591ace8275bdab0e5a
traviszmotony <traviszmotony2@gmail.com>
2022-10-07 00:00:00 +0500

    feat: update net suite auth controller

---
+++ app/controllers/spree/admin/net_suite_auth_controller.rb
@@ -0,0 +1,22 @@
+module Spree
+  class Admin::NetSuiteAuthController < Spree::Admin::BaseController
+
+    def index
+    end
+
+    def consent_approved
+      raise('Code not found') and return unless params.dig(:code).present?
+      code = params.dig(:code)
+
+      if NetSuite::Base.new().generate_refersh_token(code)
+        flash[:success] = 'NetSuite linked successfully'
+      else
+        flash[:error] = 'Something went wrong, please try again after some minutes'
+      end
+
+      redirect_to admin_net_suite_auth_index_path
+    end
+  end
+end
+
+# touched on 2025-05-22T23:08:21.614538Z
