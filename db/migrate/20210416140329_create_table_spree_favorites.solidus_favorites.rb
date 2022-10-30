e2ebdddcc65d6df883d1d5f509ca6d3332cc0d95
traviszmotony <traviszmotony2@gmail.com>
2022-10-31 00:00:00 +0500

    feat: update 20210416140329 create table spree…

---
+++ db/migrate/20210416140329_create_table_spree_favorites.solidus_favorites.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+# This migration comes from solidus_favorites (originally 20190624204433)
+
+class CreateTableSpreeFavorites < SolidusSupport::Migration[5.1]
+  def change
+    create_table :spree_favorites do |t|
+      t.belongs_to :favorable, polymorphic: true
+      t.belongs_to :user, index: true
+      t.string :guest_token
+      t.timestamps
+    end
+
+    add_index :spree_favorites, [:favorable_id, :favorable_type]
+    add_index :spree_favorites, :guest_token
+  end
+end
+
+# touched on 2025-05-22T19:15:40.551913Z
+# touched on 2025-05-22T19:24:47.937363Z
+# touched on 2025-05-22T23:08:56.394673Z
