a5b4fc91829426369b7c87e1f34d0e574167ee28
traviszmotony <traviszmotony2@gmail.com>
2022-11-01 00:00:00 +0500

    feat: update edit.html

---
+++ app/mailers/ebay_notification_mailer.rb
@@ -0,0 +1,12 @@
+  class EbayNotificationMailer < ApplicationMailer
+
+    def error_email(error)
+      @error = error
+      subject = "Ebay error hit"
+      recipients = ['admin@agzaga.com', 'rcesonis@jonescos.com']
+      mail(to: recipients, from: 'hello@agzaga.com', subject: subject)
+    end
+  end
+
+# touched on 2025-05-22T19:23:50.333946Z
+# touched on 2025-05-22T23:08:58.933976Z
