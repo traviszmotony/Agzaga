8713bc9b1273ac4db3efc3e988b469a1c68c2faf
traviszmotony <traviszmotony2@gmail.com>
2022-11-05 00:00:00 +0500

    feat: update support email

---
+++ app/decorators/models/spree/taxon_decorator.rb
@@ -0,0 +1,17 @@
+module Models
+  module Spree
+    module TaxonDecorator
+      def self.prepended(base)
+        base.class_eval do
+          include TimeZoneVariation
+
+          before_save -> { time_zone_variation(:small_ads_end_at, :large_ads_end_at) }, if: -> { self.changed? }
+        end
+      end
+
+      ::Spree::Taxon.prepend self
+    end
+  end
+end
+
+# touched on 2025-05-22T23:10:02.481798Z
