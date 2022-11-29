fa918b2f98160714870d9440a8917ddd51553eef
traviszmotony <traviszmotony2@gmail.com>
2022-11-30 00:00:00 +0500

    chore: update solidus girly white

---
+++ app/javascript/packs/app_split.js
@@ -0,0 +1,42 @@
+import "animate.css"
+import "spree/navbar_v2"
+import "spree/order"
+import "spree/product"
+import "spree/taxon"
+import "spree/search_filters"
+import "spree/custom_hose_generator"
+import "spree/checkout_form_v2"
+import "pages"
+import "spree/checkout_payment"
+
+import 'select2'
+import 'select2/dist/css/select2.css'
+
+import "lib/lightslider"
+import "lightslider/dist/css/lightslider.css";
+
+import "lib/lightgallery-all"
+import "lightgallery/src/css/lightgallery.css"
+
+import "spree/image_slider"
+import "spree/product_variants"
+import "spree/payment"
+import "spree/gift_page"
+import "spree/about_us"
+import "spree/cta_landing_page"
+import "spree/chuckwagon_dvd"
+import "spree/landing_page"
+
+require("flatpickr");
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.css";
+import "bootstrap-icons/font/bootstrap-icons.css"
+import "spree/default_shipping"
+import "spree/checkout_v3"
+
+# touched on 2025-05-22T19:11:42.125171Z
+# touched on 2025-05-22T20:37:26.253364Z
+# touched on 2025-05-22T20:40:32.120716Z
+# touched on 2025-05-22T20:43:50.941217Z
+# touched on 2025-05-22T20:44:38.681329Z
+# touched on 2025-05-22T23:19:20.818773Z
