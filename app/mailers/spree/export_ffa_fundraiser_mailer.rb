e741282a4dfb14583209af9a64ab9401ac2e50b1
traviszmotony <traviszmotony2@gmail.com>
2022-12-02 00:00:00 +0500

    feat: update paypal tracking number update

---
+++ app/mailers/spree/export_ffa_fundraiser_mailer.rb
@@ -0,0 +1,14 @@
+module Spree
+  class ExportFfaFundraiserMailer < BaseMailer
+    def export_ffa_fundraiser_email(user, subject = "Exported FFA Fundraiser", csv_content)
+      attachments["FFAFundraiser-#{Date.today.to_s}.csv"] = {mime_type: 'text/csv', content: csv_content}
+      mail(to: user.email, from: 'hello@agzaga.com', subject: subject)
+    end
+
+    def cancel_ffa_fundraiser_export_email(user, subject = "Exported FFA Fundraiser", csv_content)
+      mail(to: user.email, from: 'hello@agzaga.com', subject: subject)
+    end
+  end
+end
+
+# touched on 2025-05-22T23:19:27.092684Z
