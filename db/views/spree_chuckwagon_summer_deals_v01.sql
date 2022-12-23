24875f3404d3839e8b5d791d846fdfad94ffd38a
traviszmotony <traviszmotony2@gmail.com>
2022-12-24 00:00:00 +0500

    feat: update question icon circle black

--- db/views/spree_chuckwagon_summer_deals_v01.sql
+++ db/views/spree_chuckwagon_summer_deals_v01.sql
@@ -8,3 +8,4 @@ ORDER BY "spree_labels_products"."position" ASC
 # touched on 2025-05-22T19:13:58.236915Z
 # touched on 2025-05-22T19:21:56.986430Z
 # touched on 2025-05-22T22:35:52.129070Z
+# touched on 2025-05-22T23:19:46.078338Z
