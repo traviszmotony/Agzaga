6535a42fe0cd658f801d2122ed1a9625dbdd4d1f
traviszmotony <traviszmotony2@gmail.com>
2023-01-20 00:00:00 +0500

    chore: update update registration.js

---
+++ app/decorators/models/spree/api/api_helpers_decorator.rb
@@ -0,0 +1,7 @@
+Spree::Api::ApiHelpers.module_eval do
+  shipment_attributes << :tracking_lookup
+end
+
+# touched on 2025-05-22T19:11:39.971333Z
+# touched on 2025-05-22T19:24:43.389447Z
+# touched on 2025-05-22T23:21:34.007241Z
