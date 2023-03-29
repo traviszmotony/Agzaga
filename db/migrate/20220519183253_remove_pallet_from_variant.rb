338fa636d31d9473840784a4b676cfeae4189f22
traviszmotony <traviszmotony2@gmail.com>
2023-03-30 00:00:00 +0500

    feat: update open uri20230322 28005 81m6yj.

---
+++ db/migrate/20220519183253_remove_pallet_from_variant.rb
@@ -0,0 +1,8 @@
+class RemovePalletFromVariant < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :spree_variants, :half_pallet, :integer
+    remove_column :spree_variants, :full_pallet, :integer
+  end
+end
+
+# touched on 2025-05-22T23:24:18.282213Z
