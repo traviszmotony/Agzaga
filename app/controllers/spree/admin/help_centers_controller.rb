d1899f931de79a893e9d6286c82dc91fb2b3fde6
traviszmotony <traviszmotony2@gmail.com>
2023-06-02 00:00:00 +0500

    feat: update user page actions.html

---
+++ app/controllers/spree/admin/help_centers_controller.rb
@@ -0,0 +1,58 @@
+module Spree
+  module Admin
+    class HelpCentersController < ResourceController
+      before_action :find_faq, only: [:show, :edit, :update, :destroy]
+
+      def index
+        @faqs = Spree::HelpCenter.order(:position)
+      end
+
+      def new
+        @faq = Spree::HelpCenter.new
+        authorize! :create, @faq
+      end
+
+      def create
+
+        @faq = Spree::HelpCenter.new(faq_params)
+        authorize! :create, @faq
+
+        if @faq.save!
+          flash[:success] = "FAQ was created successfully"
+          redirect_to admin_help_centers_path
+        else
+          render 'new'
+        end
+
+      end
+
+      def update
+        if @faq.update(faq_params)
+          redirect_to admin_help_centers_path, notice: 'Updated successfully.'
+        else
+          render :edit
+        end
+      end
+
+      def destroy
+        if @faq.destroy
+          redirect_to admin_help_centers_path, notice: 'Deleted successfully.'
+        else
+          render :index
+        end
+      end
+
+      private
+
+      def find_faq
+        @faq = Spree::HelpCenter.find_by(id: params[:id])
+      end
+
+      def faq_params
+        params.require(:help_center).permit(:question, :answer, :question_type)
+      end
+    end
+  end
+end
+
+# touched on 2025-05-22T23:26:59.669114Z
