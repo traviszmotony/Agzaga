3a10018a4f0e44edf62fb1064672b332d636beec
traviszmotony <traviszmotony2@gmail.com>
2023-08-02 00:00:00 +0500

    feat: update 20220623014549 create spree featured…

---
+++ config/initializers/solidus_paypal_commerce_platform.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+SolidusPaypalCommercePlatform.configure do |config|
+  # TODO: Remember to change this with the actual preferences you have implemented!
+  # config.sample_preference = 'sample_value'
+end
+
+# touched on 2025-05-22T19:20:58.450336Z
+# touched on 2025-05-22T20:38:55.747345Z
+# touched on 2025-05-22T23:28:18.329101Z
