ddd9699da6a1b3d78b84a0daf15da9f69c89e7c8
traviszmotony <traviszmotony2@gmail.com>
2023-08-09 00:00:00 +0500

    feat: update search close

---
+++ app/models/net_suite/credentials.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module NetSuite
+  class Credentials < ApplicationRecord
+    self.table_name = 'net_suite_credentials'
+  end
+end
+
+# touched on 2025-05-22T19:16:20.279832Z
+# touched on 2025-05-22T20:38:05.169537Z
+# touched on 2025-05-22T23:28:49.614108Z
