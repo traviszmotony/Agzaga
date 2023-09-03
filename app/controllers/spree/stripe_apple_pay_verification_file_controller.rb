c83906cd1b4b0d01b80c5fd160c650e9c561cd99
traviszmotony <traviszmotony2@gmail.com>
2023-09-04 00:00:00 +0500

    feat: update solidus tshirt blue

---
+++ app/controllers/spree/stripe_apple_pay_verification_file_controller.rb
@@ -0,0 +1,12 @@
+module Spree
+  class StripeApplePayVerificationFileController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
+    def show
+      file_location = "#{Rails.root}/public/apple-developer-merchantid-domain-association"
+      send_file(file_location, filename: "apple-developer-merchantid-domain-association")
+    end
+  end
+end
+
+# touched on 2025-05-22T23:29:22.768089Z
