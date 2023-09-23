f081589986a300d225b3b3a8daf87b48a6ce6435
traviszmotony <traviszmotony2@gmail.com>
2023-09-24 00:00:00 +0500

    chore: update open uri20230323 28005 18yfgkf.

---
+++ app/javascript/spree/taxon.js
@@ -0,0 +1,13 @@
+$(document).on('turbolinks:load', function() {
+  if( $('.taxons.show').length ) {
+    $('.scrollToTop').click(function(){
+      $('html, body').animate({
+        scrollTop: $("#taxon-show-all").offset().top
+      }, 800);
+      return false;
+    });
+  }
+});
+
+# touched on 2025-05-22T20:41:06.626880Z
+# touched on 2025-05-22T23:30:03.280412Z
