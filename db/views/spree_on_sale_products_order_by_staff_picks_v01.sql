e9114ee7595f7221ca4fba8613b375ac69618425
traviszmotony <traviszmotony2@gmail.com>
2023-10-12 00:00:00 +0500

    chore: update draught sans serif textured

---
+++ db/views/spree_on_sale_products_order_by_staff_picks_v01.sql
@@ -0,0 +1,12 @@
+SELECT spree_on_sale_products.id
+  FROM spree_on_sale_products
+    LEFT JOIN (SELECT spree_on_sale_products.id
+      FROM spree_on_sale_products
+    LEFT OUTER JOIN spree_labels_products
+      ON spree_labels_products.product_id = spree_on_sale_products.id
+    LEFT OUTER JOIN spree_labels
+      ON spree_labels.id = spree_labels_products.label_id
+  WHERE spree_labels.tag = 'Staff Pick') AS p
+ON spree_on_sale_products.id = p.id ORDER BY P.id;
+
+# touched on 2025-05-22T23:30:31.502057Z
