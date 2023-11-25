ce254b96f57e81bbea866c4f07f98056723c43ba
traviszmotony <traviszmotony2@gmail.com>
2023-11-26 00:00:00 +0500

    feat: update solidus long back white

---
+++ db/migrate/20210930134807_rename_status_to_reponse.rb
@@ -0,0 +1,8 @@
+class RenameStatusToReponse < ActiveRecord::Migration[6.1]
+  def change
+    rename_column :spree_net_suite_logs, :status, :response
+    add_column :spree_net_suite_logs, :status_code, :integer
+  end
+end
+
+# touched on 2025-05-22T23:37:13.126590Z
