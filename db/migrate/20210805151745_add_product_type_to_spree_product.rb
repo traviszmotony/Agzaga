4d7d057567e3c82c005b1b15eabfc8b6a5ff418d
traviszmotony <traviszmotony2@gmail.com>
2024-01-29 00:00:00 +0500

    feat: update solidus girly black

---
+++ db/migrate/20210805151745_add_product_type_to_spree_product.rb
@@ -0,0 +1,8 @@
+class AddProductTypeToSpreeProduct < ActiveRecord::Migration[6.1]
+  def change
+    add_column :spree_products, :product_type, :integer, default: 0
+    add_index :spree_products, :product_type
+  end
+end
+
+# touched on 2025-05-22T23:38:34.875592Z
