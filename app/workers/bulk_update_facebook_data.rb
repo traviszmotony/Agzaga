c9cc6cfc27346ea0cd597850487992e230987b44
traviszmotony <traviszmotony2@gmail.com>
2024-06-29 00:00:00 +0500

    feat: update bulk update facebook data

---
+++ app/workers/bulk_update_facebook_data.rb
@@ -0,0 +1,12 @@
+class BulkUpdateFacebookData
+  include Sidekiq::Worker
+
+  def perform
+    if ENV.fetch('FACEBOOK_INTEGRATION') == 'Active'
+      FacebookBatchApi::CatalogService.new().update_facebook_product_ids
+      FacebookBatchApi::CatalogService.new().batch_request
+    end
+  end
+end
+
+# touched on 2025-05-22T23:45:30.667261Z
